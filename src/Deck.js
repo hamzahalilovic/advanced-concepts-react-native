4ee665e8a0823622ecd5b61da52d239f6146a73d
Hamza Halilovic <hamzahalilovic.12@gmail.com>
2019-09-26 18:55:08 +0200

    commit

--- src/Deck.js
+++ src/Deck.js
@@ -1,7 +1,24 @@
 import React, { Component } from "react";
-import { View, Animated } from "react-native";
+import { View, Animated, PanResponder } from "react-native";
 
 class Deck extends Component {
+  constructor(props) {
+    super(props);
+    //declare position default value 0,0
+    const position = new Animated.ValueXY();
+
+    const panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: (event, gestureState) => true,
+      onPanResponderMove: (event, gestureState) => {
+        //set position by the move
+        position.setValue({ x: gestureState.dx, y: gestureState.dy });
+      },
+      onPanResponderRelease: (event, gestureState) => {}
+    });
+    //this.position=position;
+    this.state = { panResponder, position };
+  }
+
   renderCards() {
     return this.props.data.map(item => {
       return this.props.renderCard(item);
@@ -9,7 +26,15 @@ class Deck extends Component {
   }
 
   render() {
-    return <View>{this.renderCards()}</View>;
+    return (
+      //adding the gesture to animation
+      <Animated.View
+        style={this.state.position.getLayout()}
+        {...this.state.panResponder.panHandlers}
+      >
+        {this.renderCards()}
+      </Animated.View>
+    );
   }
 }
 export default Deck;
